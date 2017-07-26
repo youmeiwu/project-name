a35461037014d23883b3f2f0403d37510473e462
sarah <smile-wu@hotmail.com>
2017-07-26 15:35:18 +0800

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,13 @@ root 'groups#index'
 resources :groups do
   member do
     post :join
-    post :quit 
+    post :quit
   end
   resources :posts
+end
+
+namespace :account do
+  resources :groups
+  resources :posts 
 end
 end
